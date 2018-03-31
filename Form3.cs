84c8c9832188c4fe848aac8640190b6181d7ea67
liuyongshuang <724691717@qq.com>
2018-03-31 13:28:27 +0800

    Second change From3

--- Form3.cs
+++ Form3.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 using System.IO;
 
@@ -18,7 +19,10 @@ namespace chart的使用
             InitializeComponent();
            // List<int> txData2 = new List<int>() { 2011, 2012, 2013, 2014, 2015, 2016 };
            // List<int> tyData2 = new List<int>() { 9, 6, 7, 4, 5, 4 };
-            List<int> txData3 = new List<int>() { 2012 };
+            int count=0;
+            int[] data1 =new int[6];
+            int[] data2 = new int[6];
+            List<int> txData3 = new List<int>() { 2003 };
             List<int> tyData3 = new List<int>() { 7 };
 
             string fileName = @"C:\Users\ASUS\Desktop\abcd.csv";
@@ -35,10 +39,15 @@ namespace chart的使用
 
                 String[] eachLine = new String[2];//因为知道每一行excel有2个单元格，所以string[2]  
                 eachLine = str.Split(',');//因为.csv文件是以逗号分隔单元格里数据的，所以调用分隔函数split  
-                string ser = eachLine[0];//年份  
-                string dse = eachLine[1];//价值 
-                ct.Series[0].Points.DataBindXY(int.Parse(ser),int.Parse(dse)); //添加数据                            
+                //double ser = double.Parse(eachLine[0]);//年份 
+                //double dse = double.Parse(eachLine[1]);//价值 
+                data1[count] = int.Parse(eachLine[0]);
+                data2[count] = int.Parse(eachLine[1]);
+                count++;                                         
             }
+            ct.Series[0].Points.DataBindXY(data1, data2); //添加数据
+            Thread.Sleep(1000);
+
             sr.Close();//关闭读流的对象 
             ct.Series[1].Points.DataBindXY(txData3, tyData3); 
         }
