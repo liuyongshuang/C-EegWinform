308a4942f99a9e51ea3c9a62e4adbe86c4663a36
liuyongshuang <724691717@qq.com>
2018-04-03 22:30:21 +0800

    控制机械手

---
+++ ControlHand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace 控制智能家居
+{
+    public partial class ControlHand : Form
+    {
+        public ControlHand()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serial_port();
+            if (this.button1.Text == "风扇 关")
+            {
+                MessageBox.Show("是否打开风扇", "提示");
+                this.button1.Text = "风扇 开";
+            }
+            else
+            {
+                MessageBox.Show("是否关闭风扇", "提示");
+                this.button1.Text = "风扇 关";
+            }
+
+        }
+        /// <summary>
+        /// SQ0:未知
+        /// SQ1:布手
+        /// SQ2:石头手
+        /// SQ3:剪刀手
+        /// </summary>
+        private void serial_port()
+        {       
+            string str = "PL0 SQ0 ONCE\r";
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+            serialPort1.Open();
+            if(serialPort1.IsOpen)
+            {
+                serialPort1.WriteLine(str);
+            }
+            serialPort1.Close();
+        }
+    }
+}
