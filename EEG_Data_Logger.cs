deed719f8670d0ef31e0d5acae87be138999a617
liuyongshuang <724691717@qq.com>
2018-04-12 20:05:06 +0800

    添加了专注度计算程序

--- EEG_Data_Logger.cs
+++ EEG_Data_Logger.cs
@@ -50,7 +50,7 @@ namespace EEG_Data_Logger
 
         }
         void Run()
-        {
+        {            
             // Handle any waiting events
             engine.ProcessEvents();
 
@@ -84,22 +84,18 @@ namespace EEG_Data_Logger
                     {
                         data[channel][i] = data[channel][i] - 8000;
                         if (data[channel][i] < -1000)
-                            GySteText = "右";
-                        //Console.WriteLine("右：" + data[channel][i]);
+                            GySteText = "右";                        
                         else if (data[channel][i] > 1000)
-                            GySteText = "左";
-                        //Console.WriteLine("左：" + data[channel][i]);
+                            GySteText = "左";                        
                         else
-                            GySteText = "中";
-                            //Console.WriteLine("中：" + data[channel][i]);
-                        //Console.WriteLine(data[channel][i]);                           
+                            GySteText = "中";                                                
                     }
                     if(channel.ToString()=="IED_AF3")
                     {
                         data_AF3[count_AF3] = data[channel][i];
                         count_AF3++;                        
                     }
-                    if (channel.ToString() == "IED_F7")
+                    if (channel.ToString()=="IED_F7")
                     {
                         data_F7[count_F7] = data[channel][i];
                         count_F7++;
@@ -142,14 +138,7 @@ namespace EEG_Data_Logger
             EEG_Logger p = new EEG_Logger();
 
             for (int i = 0; i < 5000; i++)
-            {
-                //Example for set marker to data stream and set sychronization signal
-                //if (i % 37 == 0)
-                //{
-                //    p.engine.DataSetMarker((ui   nt)p.userID, i);
-                //    p.engine.DataSetSychronizationSignal((uint)p.userID, i);
-
-                //}
+            {               
                 p.Run();
                 Thread.Sleep(10);
             }
@@ -162,11 +151,127 @@ namespace EEG_Data_Logger
             Thread t1 = new Thread(ts1);
             ThreadStart ts2 = new ThreadStart(EEG_Data);
             Thread t2 = new Thread(ts2);
+            ThreadStart ts3 = new ThreadStart(AverageBandPowers.Main_AverageBandPowers);
+            Thread t3 = new Thread(ts3);
+
             t2.Start();
-            Thread.Sleep(100);                   
-            t1.Start();                     
-                      
-            # endregion
+            t3.Start();
+            Thread.Sleep(100);
+            t1.Start();
+            # endregion   
+            //AverageBandPowers.Main_AverageBandPowers();
+        }
+    }
+
+    class AverageBandPowers
+    {
+        static double[] score =new double[320];
+        public static double answer = 0;
+        static int count_sense = 0;
+        static double sum = 0;
+        static int userID1 = -1;
+        static string filename1 = "AverageBandPowers.csv";
+        static TextWriter file = new StreamWriter(filename1, false);
+
+        static void engine_UserAdded_Event(object sender, EmoEngineEventArgs e)
+        {
+            Console.WriteLine("User Added Event has occured");
+            userID1 = (int)e.userId;
+
+            EmoEngine.Instance.IEE_FFTSetWindowingType((uint)userID1, EdkDll.IEE_WindowingTypes.IEE_HAMMING);
+        }
+
+
+        public static void Main_AverageBandPowers()
+        {            
+            for (int t = 0; t < 320;t++ )
+            {
+                score[t] = 0;
+            }
+            Console.WriteLine("===================================================================================");
+            Console.WriteLine("Example to get the average band power for a specific channel from the latest epoch.");
+            Console.WriteLine("===================================================================================");
+
+            AverageBandPowers p = new AverageBandPowers();
+
+            // create the engine
+            EmoEngine engine = EmoEngine.Instance;
+            engine.UserAdded += new EmoEngine.UserAddedEventHandler(engine_UserAdded_Event);
+            engine.Connect();
+
+            string header = "Theta, Alpha, Low_beta, High_beta, Gamma"; ;
+            file.WriteLine(header);
+            file.WriteLine("");
+
+            EdkDll.IEE_DataChannel_t[] channelList = new EdkDll.IEE_DataChannel_t[5] { EdkDll.IEE_DataChannel_t.IED_AF3, 
+                                                                                       EdkDll.IEE_DataChannel_t.IED_AF4, 
+                                                                                       EdkDll.IEE_DataChannel_t.IED_T7, 
+                                                                                       EdkDll.IEE_DataChannel_t.IED_T8, 
+                                                                                       EdkDll.IEE_DataChannel_t.IED_O1 };
+
+            while (true)
+            {
+                engine.ProcessEvents(10);
+
+                if (userID1 < 0) continue;
+
+                if (Console.KeyAvailable)
+                    break;
+
+                double[] alpha = new double[1];
+                double[] low_beta = new double[1];
+                double[] high_beta = new double[1];
+                double[] gamma = new double[1];
+                double[] theta = new double[1];
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Int32 result = engine.IEE_GetAverageBandPowers((uint)userID1, channelList[i], theta, alpha, low_beta, high_beta, gamma);
+                    if (result == EdkDll.EDK_OK)
+                    {
+                        file.Write(theta[0] + ",");
+                        file.Write(alpha[0] + ",");
+                        file.Write(low_beta[0] + ",");
+                        file.Write(high_beta[0] + ",");
+                        file.WriteLine(gamma[0] + ",");
+                       // Console.WriteLine("Theta: " + theta[0]);
+                        /////////////////////////////////////////////////
+                        ///////////////////专注度算法////////////////////
+                        /////////////////////////////////////////////////
+                        score[count_sense] = (high_beta[0] + low_beta[0]) / theta[0];
+                        count_sense++;
+                        if (count_sense == 320) { count_sense = 0; }
+                        if(score[59]!=0)
+                        {
+                            sum = 0;
+                            for(int j = 0;j < 320; j++)
+                            {
+                                sum = sum + score[j];
+                            }
+                        }
+                        //Console.WriteLine(sum);
+                        answer = Math.Sqrt(sum / 10) * 6;
+                        while (answer > 100)
+                        {
+                            answer = answer - 10;
+                        }
+
+                        if (answer <= 50)
+                        {
+                            answer = 100 * Math.Sqrt(answer / 50) * 0.5;
+                        }
+                        else
+                        {
+                            answer = 100 * 0.5 * (1 + ((answer - 50) / 50) * ((answer - 50) / 50));
+                        }
+                        answer = Math.Abs((answer - 20) / 0.8);
+                        //Console.WriteLine(answer);
+                    }
+                }
+            }
+
+            file.Close();
+            engine.Disconnect();
         }
     }
 }
