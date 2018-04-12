a2ad6f16f75541f666cf8571a18aeec5b0939ac5
liuyongshuang <724691717@qq.com>
2018-04-12 19:52:17 +0800

    窗口可以显示两条曲线或一条曲线，但与专注度的计算冲突

--- EEG.cs
+++ EEG.cs
@@ -19,6 +19,7 @@ namespace EEG_Data_Logger
 
         static int count = 0;
         static int count0 = 0;
+        static int flag = 0;
 
         //static string str = "";     
         //static int flag = 0;
@@ -41,7 +42,10 @@ namespace EEG_Data_Logger
         {
             # region 初始化chart
             DateTime time = DateTime.Now;
-           
+
+            data_af3[0] = 0;
+            data_f7[0] = 0;
+             
             chartTimer.Tick += chartTimer_Tick;
             chartDemo.DoubleClick += chartDemo_DoubleClick;
 
@@ -51,15 +55,12 @@ namespace EEG_Data_Logger
             series_af3.ChartType = SeriesChartType.Spline;
             series_f7.ChartType = SeriesChartType.Spline;
 
-            chartDemo.ChartAreas[0].AxisX.LabelStyle.Format = "HH:mm:ss";//x轴显示时间格式
+            //chartDemo.ChartAreas[0].AxisX.LabelStyle.Format = "HH:mm:ss";//x轴显示时间格式
             chartDemo.ChartAreas[0].AxisX.ScaleView.Size = 5;
             chartDemo.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
-            chartDemo.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
-            chartTimer.Start();
+            chartDemo.ChartAreas[0].AxisX.ScrollBar.Enabled = true;            
             label1.Text = "中";
-
-            timer1.Enabled = false;
-            chartTimer.Enabled = false;
+                      
 
             # endregion
         }
@@ -114,71 +115,94 @@ namespace EEG_Data_Logger
         //}
 
         private void chartTimer_Tick(object sender, EventArgs e)
-        {          
-            Series series_AF3 = chartDemo.Series[0];           
-            data_af3[count] = EEG_Logger.data_AF3[count] - 4000;         
-            chartDemo.Series[0].BorderDashStyle = ChartDashStyle.Solid;            
-            series_AF3.Points.AddXY(DateTime.Now, data_af3[count]);           
-
-            chartDemo.ChartAreas[0].AxisX.ScaleView.Position = series_AF3.Points.Count - 5;            
-            count++;
-            label1.Text = EEG_Logger.GySteText;
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
         {
+            Series series_AF3 = chartDemo.Series[0];
             Series series_F7 = chartDemo.Series[1];
-            data_f7[count0] = EEG_Logger.data_F7[count0] - 4000;
-            chartDemo.Series[1].BorderDashStyle = ChartDashStyle.Solid;
-            series_F7.Points.AddXY(DateTime.Now, data_f7[count0]);
+           
+            if (flag == 1)
+            {
+                series_F7.Enabled = false;
+                series_AF3.Enabled = true;
+                data_af3[count] = EEG_Logger.data_AF3[count] - 4000;
+                chartDemo.Series[0].BorderDashStyle = ChartDashStyle.Solid;
+                series_AF3.Points.AddXY(count, data_af3[count]);
+                chartDemo.ChartAreas[0].AxisX.ScaleView.Position = series_AF3.Points.Count - 5;
+                count++;
+            }
+            else if(flag == 4)
+            {
+                series_AF3.Enabled = false;
+                series_F7.Enabled = true;
+                data_f7[count0] = EEG_Logger.data_F7[count0] - 4000;
+                chartDemo.Series[1].BorderDashStyle = ChartDashStyle.Solid;
+                series_F7.Points.AddXY(count0, data_f7[count0]);
+                chartDemo.ChartAreas[0].AxisX.ScaleView.Position = series_F7.Points.Count - 5; 
+                count0++;
+            }
+            else if (flag == 3)
+            {
+                series_AF3.Enabled = true;
+                series_F7.Enabled = true;
 
-            chartDemo.ChartAreas[0].AxisX.ScaleView.Position = series_F7.Points.Count - 5;
-            count0++;
-        }      
+                data_af3[count] = EEG_Logger.data_AF3[count] - 4000;
+                chartDemo.Series[0].BorderDashStyle = ChartDashStyle.Solid;
+                series_AF3.Points.AddXY(count, data_af3[count]);
+                chartDemo.ChartAreas[0].AxisX.ScaleView.Position = series_AF3.Points.Count - 5;
+                //count++;
+
+                data_f7[count] = EEG_Logger.data_F7[count] - 4000;
+                chartDemo.Series[1].BorderDashStyle = ChartDashStyle.Solid;
+                series_F7.Points.AddXY(count, data_f7[count]);
+                chartDemo.ChartAreas[0].AxisX.ScaleView.Position = series_F7.Points.Count - 5;
+                count++;
+
+            }
+            else
+            {
+                series_AF3.Enabled = false;
+                series_F7.Enabled = false;
+            }
+            
+                    
+            label1.Text = EEG_Logger.GySteText;
+            label2.Text = AverageBandPowers.answer.ToString();
+        }
+      
         private void F7_CheckedChanged(object sender, EventArgs e)
-        {            
-             if(radio_F7.Checked)
-             {
-                 chartTimer.Enabled = false;
-                 Thread.Sleep(1000);                
-                 timer1.Enabled = true;
-             }
-             else
-             {
-                 timer1.Enabled = false;
-                 chartTimer.Enabled = false;
-             }
+        {
+            if (radio_F7.Checked)
+            {
+                flag = 2;
+            }
+            else
+            {
+                flag = 0;
+            }        
+             
         }
 
         private void radio_AF3_CheckedChanged(object sender, EventArgs e)
-        {           
-            //if (radio_AF3.Checked)
-            //{
-            //    timer1.Enabled = false;
-            //    Thread.Sleep(1000);
-            //    chartTimer.Enabled = true;               
-            //}
-            //else
-            //{
-            //    timer1.Enabled = false;
-            //    chartTimer.Enabled = false;
-            //}            
-                timer1.Enabled = false;
-                chartTimer.Enabled = false;           
+        {
+            if (radio_AF3.Checked)
+            {
+                flag = 1;
+            }
+            else
+            {
+                flag = 0;
+            }            
+                          
         }
 
         private void checkBox_AF3_CheckedChanged(object sender, EventArgs e)
         {
-            if(checkBox_AF3.Checked)
+            if (checkBox_AF3.Checked)
             {
-                timer1.Enabled = false;
-                Thread.Sleep(1000);
-                chartTimer.Enabled = true;
+                flag = 3;
             }
             else
             {
-                timer1.Enabled = false;
-                chartTimer.Enabled = false;
+                flag = 0;
             }
 
         }
@@ -187,15 +211,12 @@ namespace EEG_Data_Logger
         {
             if (checkBox_F7.Checked)
             {
-                chartTimer.Enabled = false;
-                Thread.Sleep(1000);
-                timer1.Enabled = true;
+                flag = 4;
             }
             else
             {
-                timer1.Enabled = false;
-                chartTimer.Enabled = false;
+                flag = 0;
             }
-        }
+        }        
     }
 }
